5f6c0004285553471a539443ff3af60411aada2a
lfolham <lucasfolha@icloud.com>
2023-07-13 16:17:38 -0300

    setup database/model

---
+++ app/backend/src/database/models/TeamsMode.ts
@@ -0,0 +1,33 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeTeam extends Model<InferAttributes<SequelizeTeam>, InferCreationAttributes<SequelizeTeam>> {
+    declare id: CreationOptional<number>;
+    declare teamName: string;
+}
+
+SequelizeTeam.init(
+  {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    teamName: DataTypes.STRING,
+  },
+  {
+    tableName: 'teams',
+    underscored: true,
+    timestamps: false,
+    sequelize: db,
+  },
+);
+
+export default SequelizeTeam;
