7ea57e6dc80f7d2ce475098330d57ea8a3cffbbe
lfolham <lucasfolha@icloud.com>
2023-07-18 14:03:27 -0300

    setup login Controller

--- app/backend/src/controllers/AuthController.ts
+++ app/backend/src/controllers/AuthController.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express';
 import * as bcrypt from 'bcryptjs';
+import mapStatusHTTP from '../utils/mapStatusHTTP';
 import { IUser } from '../Interfaces/Users/IUser';
 import { IUserModel } from '../Interfaces/Users/IUserModel';
 import JwtUtils from '../utils/jwtUtils';
 import UsersModel from '../models/UsersModel';
+import UserService from '../services/UsersService';
 
 export default class AuthController {
   private userModel: IUserModel = new UsersModel();
+  private userService = new UserService();
   private jwtUtils = new JwtUtils();
 
   constructor(userModel: IUserModel) {
@@ -35,5 +38,16 @@ export default class AuthController {
     return res.status(200).json({
       token,
     });
+  }
+
+  public async getById(req: Request, res: Response) {
+    const { id } = req.params;
+    const serviceResponse = await this.userService.getById(Number(id));
+
+    if (serviceResponse.status !== 'SUCCESSFUL') {
+      return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);
+    }
+
+    res.status(200).json(serviceResponse.data);
   }
 }
