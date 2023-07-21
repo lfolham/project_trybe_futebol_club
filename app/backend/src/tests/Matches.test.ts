9f46173c599561074d509da97d736b6b5078d21e
lfolham <lucasfolha@icloud.com>
2023-07-21 14:38:21 -0300

    task 22 - tests

--- app/backend/src/tests/Matches.test.ts
+++ app/backend/src/tests/Matches.test.ts
@@ -7,7 +7,8 @@ import { app } from '../app';
 
 import Match from '../database/models/MatchesModel'
 
-import { matches } from './mocks/matches.mock';
+import { createMatch, matches, matchesEnd, matchesInProgress, mockNewMatch } from './mocks/matches.mock';
+import JwtUtils from '../utils/jwtUtils';
 
 chai.use(chaiHttp);
 
@@ -28,5 +29,54 @@ describe('Matches test', async function() {
     expect(status).to.equal(200);
     expect(body).to.deep.equal(matches);
     })
+
+    it('GET/matches matches in progress - successfull', async function() {
+        sinon.stub(Match, 'findAll').resolves(matchesInProgress as any)
+
+    const { status, body } = await chai
+    .request(app)
+    .get('/matches?inProgress=true');
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(matchesInProgress);
+    })
+
+    it('GET/matches finished matches  - successfull', async function() {
+        sinon.stub(Match, 'findAll').resolves(matchesEnd as any)
+
+    const { status, body } = await chai
+    .request(app)
+    .get('/matches?inProgress=false');
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(matchesEnd);
+    })
+
+    it('POST /matches creates a new match - successful', async function() {
+    sinon.stub(Match, 'findAll').resolves(createMatch as any);
+     sinon.stub(JwtUtils.prototype, 'verify').returns(createMatch);
+
+     const { status, body } = await chai
+    .request(app)
+    .post('/matches')
+    .set('Authorization', 'Bearer token')
+    .send(mockNewMatch);
+
+     expect(status).to.equal(201);
+     expect(body).to.deep.equal(createMatch);
+    });
+
+    it('PATCH/matches finis a match - successful', async function() {
+    sinon.stub(Match, 'findAll').resolves(createMatch as any);
+    sinon.stub(JwtUtils.prototype, 'verify').returns(createMatch);
+
+     const { status, body } = await chai
+    .request(app)
+    .patch('/matches/41/finish')
+    .set('Authorization', 'Bearer token')
+
+     expect(status).to.equal(201);
+     expect(body).to.deep.equal({ message: 'Finished' });
+    });
 
 })
